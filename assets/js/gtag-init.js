f1868fc9557dc3476ba149e30de713723974f452
BILLIONAIRS <admin@billionairs.luxury>
2026-02-18 03:34:54 +0100

    Revert "Security: Remove unsafe-inline from CSP - extract all inline scripts to external files"
    This reverts commit 5d71fc28a36b374fddd2e564b67f3e11fca03b69.

--- assets/js/gtag-init.js
+++
@@ -1,5 +0,0 @@
-// Google Analytics (gtag)
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-80P17E7RZK');
--- assets/js/https-redirect.js
+++
@@ -1,4 +0,0 @@
-// Force HTTPS in production
-if (location.protocol !== 'https:' && location.hostname !== 'localhost' && location.hostname !== '127.0.0.1') {
-    location.replace('https://' + location.hostname + location.pathname + location.search);
-}
